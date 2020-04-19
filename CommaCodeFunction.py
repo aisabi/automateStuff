d8284f688d308a54e312f4387cd592c475b9736a
Aissa <aissa.abid@laposte.net>
2020-04-19 19:57:54 +0200

    add solution practice project lists #1 p102

---
+++ CommaCodeFunction.py
@@ -0,0 +1,17 @@
+# RESULTAT ATTENDU 'apples, bananas, tofu, and cats' page 102
+def commaCode(valeur):
+	i = 1
+	while i <len(spam):
+		spam.insert(i,', ')
+		i = i + 2
+	spam.insert(-1,'AND ')
+	phrase =''
+	for i in range(len(spam)):
+		phrase = phrase+spam[i]
+	print(phrase)
+
+
+
+spam = ['apples', 'bananas', 'tofu', 'cats']
+
+commaCode(spam)
